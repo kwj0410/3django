2a56ab22466e4894322e57c98238e8656f49d32a
kwj0410 <jake0410@naver.com>
2020-07-04 00:05:14 +0900

    3django

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jake0\onedrive\바탕화~3-desktop-fuc7eps\장고시작\3\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
